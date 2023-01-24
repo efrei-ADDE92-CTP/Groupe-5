66128fb9de7bbf710d881b8aa90cbdfa4080ba0f
xulaing <khauv@live.fr>
2023-01-24 11:25:55 +0100

    api

---
+++ api.py
@@ -0,0 +1,24 @@
+import requests
+import os
+import pandas as pd
+from flask import Flask
+from flask import request, jsonify
+import joblib
+
+app = Flask(__name__)
+
+with open('iris_knn.pkl', 'rb') as file:
+    model = joblib.load(file)
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json(force=True)
+    df = pd.DataFrame(data, index=[0])
+
+    prediction = model.predict(df)
+    return jsonify({'prediction': list(prediction)})
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
