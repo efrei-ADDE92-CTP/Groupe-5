83e142f070b810087cfcfa004ad067072523c96a
luciebottin <lucie.bottin@efrei.net>
2023-02-22 17:12:30 +0100

    server empty

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,12 @@
-FROM python:3.8-slim
-COPY requirements.txt .
-COPY api.py .
-COPY iris_knn.pkl .
-RUN pip install --no-cache-dir -r requirements.txt
-EXPOSE 5000
-ENV FLASK_APP api.py
-CMD flask run --host 0.0.0.0 --port 5000
-ENTRYPOINT [ "/luciebottin/iris-docker" ]
+FROM python:3.7.16-slim-bullseye
+
+WORKDIR /app
+
+COPY requirements.txt ./
+RUN python3 -m pip install --no-cache-dir -r requirements.txt
+
+COPY . .
+
+COPY api.py /var/server/api.py
+
+CMD ["python3", "/var/server/api.py"]
--- api.py
+++ api.py
@@ -3,6 +3,7 @@ import pandas as pd
 from flask import Flask
 from flask import request, jsonify
 import joblib
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 
 app = Flask(__name__)
 
@@ -19,5 +20,17 @@ def predict():
     return jsonify({'prediction': list(prediction)})
 
 
+# Define a counter to track the number of requests
+REQUEST_COUNT = Counter('myapp_request_count', 'Number of requests received')
+
+@app.route('/metrics')
+def metrics():
+    # Increment the request count
+    REQUEST_COUNT.inc()
+
+    # Return the latest metrics as a string in Prometheus text format
+    return generate_latest(), 200, {'Content-Type': CONTENT_TYPE_LATEST}
+
+
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
